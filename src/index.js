d98ff764da3cafd0bcd3763247195b06fca5e999
Sokolov V <sokolov.stb@gmail.com>
2018-09-28 11:02:42 +0300

    implement getZeorsCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+
+  for(let i = 1, len = number; i < len; i++){
+    result += Math.floor(number / Math.pow(5,i));
+
+    if(Math.floor(number / (Math.pow(5,i))) == 0) break;
+  }
+
+  return result;
 }
