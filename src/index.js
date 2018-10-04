42b1033bd27c176bca760b1fe20f3e9b7f977d05
Sokolov V <sokolov.stb@gmail.com>
2018-10-04 10:45:21 +0300

    fix after webinar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function getZerosCount(number) {
   let result = 0;
 
-  for(let i = 1, len = number; i < len; i++){
-    result += Math.floor(number / Math.pow(5,i));
-
-    if(Math.floor(number / (Math.pow(5,i))) == 0) break;
+  for(let i = 5; number / i >= 1; i *= 5){
+    result += Math.floor(number / i);
   }
 
   return result;
